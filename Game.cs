0237817873922f702b30381afaa6e42e01dcd167
haedrichowen <owen.haedrich@mohawkcollege.ca>
2025-03-18 11:09:11 -0400

    Added template for changing the primary game state (Menu, Play, GameOver).

--- Game.cs
+++ Game.cs
@@ -10,23 +10,55 @@ namespace MohawkGame2D;
 /// </summary>
 public class Game
 {
-    // Place your variables here:
+    enum gameState
+    {
+        Menu,
+        Play,
+        GameOver
+    }
 
+    gameState state = gameState.Menu;
 
-    /// <summary>
-    ///     Setup runs once before the game loop begins.
-    /// </summary>
     public void Setup()
     {
 
     }
 
-    /// <summary>
-    ///     Update runs every frame.
-    /// </summary>
     public void Update()
     {
-
+        switch (state)
+        {
+            case gameState.Menu:
+                Menu();
+                if (Input.IsKeyboardKeyPressed(KeyboardInput.Enter))
+                {
+                    state = gameState.Play;
+                }
+                break;
+            case gameState.Play:
+                Play();
+                break;
+            case gameState.GameOver:
+                GameOver();
+                if (Input.IsKeyboardKeyPressed(KeyboardInput.Enter))
+                {
+                    state = gameState.Menu;
+                }
+                break;
+        }
     }
+
+    public void Menu()
+    {
+    }
+
+    public void Play()
+    {
+    }
+
+    public void GameOver()
+    {
+    }
+
 }
 
