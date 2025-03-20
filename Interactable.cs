390c093217734428ce19bb1631e358b9fa943f2e
haedrichowen <owen.haedrich@mohawkcollege.ca>
2025-03-20 13:12:56 -0400

    Add ItemHolder class to facilitate a home position for the items. Add the cauldron

--- Interactable.cs
+++ Interactable.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
+﻿using System.Numerics;
 using MohawkGame2D;
 
 namespace Collaborative_2D_Game_Project
@@ -9,7 +6,7 @@ namespace Collaborative_2D_Game_Project
     class Interactable
     {
         public Vector2 position;
-        public Vector2 homePosition;
+        public Vector2 homePosition = Vector2.NegativeInfinity;
         public Texture2D texture; // The texture of the interactable
         public Material? material; // The material if the interactable has one
         bool moveable = false; // Whether the interactable can be moved
