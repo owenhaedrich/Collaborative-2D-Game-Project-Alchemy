e14891ec5e1cf79aef69aa0ad81138723d656d80
StitchOhana13 <Marcus13Faria@icloud.com>
2025-03-26 15:41:38 -0400

    add Background screen, and lit and unlit Cauldron assets

--- Game.cs
+++ Game.cs
@@ -1,5 +1,6 @@
 ﻿// Include the namespaces (code libraries) you need below.
 using System;
+using System.Linq;
 using System.Numerics;
 using Collaborative_2D_Game_Project;
 
@@ -11,6 +12,7 @@ namespace MohawkGame2D;
 /// </summary>
 public class Game
 {
+    // Game State
     enum gameState
     {
         Menu,
@@ -20,9 +22,37 @@ public class Game
 
     gameState state = gameState.Menu;
 
+    // Shelves
+    const int shelfWidth = 4;
+    const int shelfHeight = 5;
+
+    // Cauldron
+    Vector2 cauldronPourPosition = new Vector2(400, 200);
+    Vector2 cauldronPosition = new Vector2(400, 300);
+    float cauldronRadius = 50;
+
+    // Physics
+    Vector2 gravity = Vector2.UnitY * 10;
+
+    // Game Objects
+    Interactable[] bottles = [new Interactable(Interactable.EmptyBottle, new Vector2(100,100)), new Interactable(Interactable.EmptyBottle, new Vector2(100, 300)), new Interactable(Interactable.EmptyBottle, new Vector2(100, 500))];
+    ItemHolder[] shelves = new ItemHolder[shelfWidth * shelfHeight * 2];
+
     public void Setup()
     {
+        Window.SetSize(800, 600);
 
+        // Generate Shelves
+        int i = 0;
+        for (int x = 0; x < shelfWidth; x++)
+        {
+            for (int y = 0; y < shelfHeight; y++)
+            {
+                shelves[i] = (new ItemHolder(new Vector2(50 + x * 75, 100 + y * 100)));
+                shelves[i + shelves.Length/2] = (new ItemHolder(new Vector2(510 + x * 75, 100 + y * 100)));
+                i++;
+            }
+        }
     }
 
     public void Update()
@@ -31,7 +61,7 @@ public class Game
         {
             case gameState.Menu:
                 Menu();
-                if (Input.IsKeyboardKeyPressed(KeyboardInput.Enter))
+                if (Input.IsKeyboardKeyPressed(KeyboardInput.Enter) || Input.IsMouseButtonPressed(MouseInput.Left))
                 {
                     state = gameState.Play;
                 }
@@ -41,7 +71,7 @@ public class Game
                 break;
             case gameState.GameOver:
                 GameOver();
-                if (Input.IsKeyboardKeyPressed(KeyboardInput.Enter))
+                if (Input.IsKeyboardKeyPressed(KeyboardInput.Enter) || Input.IsMouseButtonPressed(MouseInput.Left))
                 {
                     state = gameState.Menu;
                 }
@@ -52,12 +82,248 @@ public class Game
     public void Menu()
     {
         Window.ClearBackground(Color.OffWhite);
-        Console.WriteLine("Click to play");
     }
 
     public void Play()
     {
-        Console.WriteLine(Material.Combine([Material.Water, Material.Earth]).name);
+        Window.ClearBackground(Color.OffWhite);
+
+        //Graphics.Draw(gameBackground);
+
+        ManageCauldron();
+        MoveInteractables();
+        ManageItemHolders();
+    }
+
+    public void ManageItemHolders()
+    {
+        foreach (ItemHolder itemHolder in shelves)
+        {
+            if (itemHolder.item is not null)
+            {
+                if (itemHolder.item.homePosition != itemHolder.position)
+                {
+                    itemHolder.item = null;
+                }
+            }
+            itemHolder.Render();
+        }
+    }
+
+    public void ManageCauldron()
+    {
+        Draw.FillColor = Color.Black;
+        Draw.Circle(cauldronPosition, cauldronRadius);
+        Draw.FillColor = Color.Blue;
+        Draw.Circle(cauldronPourPosition, cauldronRadius);
+        Interactable[] bottlesAboveCauldron = new Interactable[4];
+        Interactable[] bottlesInCauldron = new Interactable[4];
+
+        // Add materials above the cauldron up to the max capacity
+        for (int i = 0; i < bottlesAboveCauldron.Length; i++)
+        {
+            if (bottlesAboveCauldron[i] is null)
+            {
+                foreach (Interactable bottle in bottles)
+                {
+                    if (bottle.homePosition == cauldronPourPosition && !bottlesAboveCauldron.Contains(bottle))
+                    {
+                        bottlesAboveCauldron[i] = bottle;
+                        break;
+                    }
+                    if (bottle.homePosition == cauldronPosition && !bottlesInCauldron.Contains(bottle))
+                    {
+                        bottlesInCauldron[i] = bottle;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (Input.IsMouseButtonPressed(MouseInput.Left) && Vector2.Distance(Input.GetMousePosition(), cauldronPosition) < cauldronRadius)
+        {
+            foreach (Interactable bottle in bottlesAboveCauldron)
+            {
+                if (bottle is not null)
+                {
+                    bottle.homePosition = cauldronPosition;
+                }
+            }
+        }
+
+        // Combine bottles in the cauldron once they all move inside
+        bool allInCauldron = true;
+        int freeCount = 0;
+        foreach (Interactable bottle in bottlesInCauldron)
+        {
+            if (bottle is not null)
+            {
+                if (bottle.free)
+                {
+                    freeCount++;
+                }
+                else
+                {
+                    if (Vector2.Distance(bottle.position + Interactable.bottleSize / 2, cauldronPosition) > cauldronRadius)
+                    {
+                        allInCauldron = false;
+                        break;
+                    }
+                }
+                
+            }
+            else
+            {
+                freeCount++;
+            }
+        }
+
+        if (allInCauldron && freeCount < bottlesInCauldron.Length)
+        {
+            foreach (Interactable bottle in bottlesInCauldron)
+            {
+                if (bottle is not null)
+                {
+                    bottle.Free();
+                }
+            }
+
+            for (int i = 0; i < bottles.Length; i++)
+            {
+                if (bottles[i].free)
+                {
+                    bottles[i] = CombineBottles(bottlesInCauldron);
+                    break;
+                }
+            }
+        }
+    }
+
+    public Interactable CombineBottles(Interactable?[] combinationBottles)
+    {
+        Material?[] cauldronMaterials = new Material[4];
+        for (int i = 0; i < combinationBottles.Length; i++)
+        {
+            if (combinationBottles[i] is not null)
+            {
+                cauldronMaterials[i] = combinationBottles[i].material;
+            }
+        }
+
+        Interactable combinedBottle = new Interactable(Interactable.EmptyBottle, cauldronPosition - Interactable.bottleSize / 2, Material.Combine(cauldronMaterials));
+        combinedBottle.homePosition = cauldronPourPosition;
+        return combinedBottle;
+    }
+
+    public void MoveInteractables()
+    {
+        foreach (Interactable interactable in bottles)
+        {
+            Vector2 interactableSize = new Vector2(interactable.texture.Width, interactable.texture.Height);
+            bool closeToInteractable = Vector2.Distance(Input.GetMousePosition(), interactable.position + interactableSize / 2) < 50;
+            interactable.Render();
+            if (Input.IsMouseButtonPressed(MouseInput.Left) && closeToInteractable)
+            {
+                interactable.Interact();
+                break;
+            }
+
+            // Moving interactables move with the mouse
+            if (interactable.moving)
+            {
+                if (!Input.IsMouseButtonDown(MouseInput.Left))
+                {
+                    interactable.moving = false;
+                }
+                else
+                {
+                    interactable.position = Input.GetMousePosition() - interactableSize / 2;
+                }
+            }
+            else
+            {
+                // Interactables move back to their home position if they are not being moved
+                if (interactable.homePosition != Vector2.NegativeInfinity)
+                {
+                    float distanceToHome = Vector2.Distance(interactable.position + interactableSize / 2, interactable.homePosition);
+                    if (distanceToHome > 3)
+                    {
+                        float newX = float.Lerp(interactable.position.X, interactable.homePosition.X - interactableSize.X / 2, 0.03f);
+                        float newY = float.Lerp(interactable.position.Y, interactable.homePosition.Y - interactableSize.Y / 2, 1/distanceToHome * 2.7f);
+                        interactable.position = new Vector2(newX, newY);
+                    }
+                }
+            }
+
+            // Set item holder to not full when its item gets a new home
+
+
+            // Check if the interactable is near an item holder when released. Add the interactable to the item holder if it is not full.
+            if (Input.IsMouseButtonReleased(MouseInput.Left))
+            {
+                ItemHolder closestItemHolder = null;
+                foreach (ItemHolder itemHolder in shelves)
+                {
+                    Vector2 itemHolderSize = new Vector2(itemHolder.texture.Width, itemHolder.texture.Height);
+                    bool closeToItemHolder = Vector2.Distance(interactable.position + interactableSize / 2, itemHolder.position + itemHolderSize / 2) < 50;
+                    if (closeToItemHolder && itemHolder.item is null)
+                    {
+                        closestItemHolder = itemHolder;
+                    }
+                }
+
+                if (closestItemHolder != null)
+                {
+                    interactable.homePosition = closestItemHolder.position;
+                    closestItemHolder.item = interactable;
+                }
+            }
+
+            // Check if the interactable is released over the cauldron - ignore if the home position is tha cauldron
+            if (Vector2.Distance(interactable.position + interactableSize / 2, cauldronPourPosition) < cauldronRadius && Input.IsMouseButtonReleased(MouseInput.Left) && interactable.homePosition != cauldronPosition)
+            {
+                interactable.homePosition = cauldronPourPosition;
+            }
+            
+            // Interactables don't overlap, push them apart if they do
+            foreach (Interactable otherInteractable in bottles)
+            {
+                if (interactable != otherInteractable)
+                {
+                    if (Vector2.Distance(interactable.position + interactableSize / 2, otherInteractable.position + interactableSize / 2) < 50)
+                    {
+                        Vector2 direction = interactable.position - otherInteractable.position;
+                        if (direction == Vector2.Zero)
+                        {
+                            interactable.position += Vector2.One;
+                        }
+                        else
+                        {
+                            interactable.position += Vector2.Normalize(direction) * 1.1f;
+                        }                        
+                    }
+                }
+            }
+
+            // Interactables don't go off screen, push them back if they do
+            if (interactable.position.X < 0)
+            {
+                interactable.position.X = 0;
+            }
+            if (interactable.position.X > Window.Width - interactableSize.X)
+            {
+                interactable.position.X = Window.Width - interactableSize.X;
+            }
+            if (interactable.position.Y < 0)
+            {
+                interactable.position.Y = 0;
+            }
+            if (interactable.position.Y > Window.Height - interactableSize.Y)
+            {
+                interactable.position.Y = Window.Height - interactableSize.Y;
+            }
+
+        }
     }
 
     public void GameOver()
--- Material.cs
+++ Material.cs
@@ -4,31 +4,86 @@ using MohawkGame2D;
 
 namespace Collaborative_2D_Game_Project
 {
-    class Material(string name, Material[] madeFrom, int rarity = 0)
+    public class Material(string name, Material[] madeFrom, Texture2D texture = new Texture2D(), int rarity = 0)
     {
         public string name = name;
         public int rarity = rarity; // Rarity of the material, 0 being the most common and 5 being the rarest
         public Texture2D texture;
         public Material[] madeFrom = madeFrom; // The materials required to create this material
 
+        // Game Screens
+        public Texture2D gameBackground = Graphics.LoadTexture("../../../assets/Screens/Game_Background");
+
+        // Cauldron Textures
+        public Texture2D cauldronAsset = Graphics.LoadTexture("../../../assets/graphics/Cauldron");
+        public Texture2D litCauldronAsset = Graphics.LoadTexture("../../../assets/graphics/CauldronLit");
+
+        //MATERIAL TEXTURES
+        static Texture2D fireTexture = Graphics.LoadTexture("../../../assets/materials/fire.png");
+        static Texture2D bloodTexture = Graphics.LoadTexture("../../../assets/materials/blood.png");
+        static Texture2D batWingTexture = Graphics.LoadTexture("../../../assets/materials/bat wing.png");
+        static Texture2D crystalTexture = Graphics.LoadTexture("../../../assets/materials/crystal.png");
+        static Texture2D eyeballTexture = Graphics.LoadTexture("../../../assets/materials/eyeball.png");
+        static Texture2D featherTexture = Graphics.LoadTexture("../../../assets/materials/feather.png");
+        static Texture2D frogLegTexture = Graphics.LoadTexture("../../../assets/materials/frog leg.png");
+        static Texture2D iceTexture = Graphics.LoadTexture("../../../assets/materials/ice.png");
+        static Texture2D inkTexture = new Texture2D(); //Graphics.LoadTexture("../../../assets/materials/ink.png"); ---------------------
+        static Texture2D ironTexture = Graphics.LoadTexture("../../../assets/materials/iron.png");
+        static Texture2D moonlightTexture = Graphics.LoadTexture("../../../assets/materials/moonlight.png");
+        static Texture2D mushroomTexture = Graphics.LoadTexture("../../../assets/materials/mushroom.png");
+        static Texture2D paperTexture = Graphics.LoadTexture("../../../assets/materials/paper.png");
+        static Texture2D rockTexture = Graphics.LoadTexture("../../../assets/materials/rock.png");
+        static Texture2D saltTexture = Graphics.LoadTexture("../../../assets/materials/salt.png");
+        static Texture2D snowTexture = Graphics.LoadTexture("../../../assets/materials/snow.png");
+        static Texture2D sunTexture = Graphics.LoadTexture("../../../assets/materials/sun.png");
+        static Texture2D waterTexture = Graphics.LoadTexture("../../../assets/materials/water.png");
+        static Texture2D woodTexture = Graphics.LoadTexture("../../../assets/materials/wood.png");
+
         //Basic Materials
         public static Material Junk = new Material("Junk", []); // Junk is made from unsuccessful combinations
-        public static Material Fire = new Material("Fire", []);
-        public static Material Water = new Material("Water", []);
-        public static Material Earth = new Material("Earth", []);
-        public static Material Air = new Material("Air", []);
+        public static Material Fire = new Material("Fire", [], fireTexture);
+        public static Material Blood = new Material("Earth", [], bloodTexture);
+        public static Material BatWing = new Material ("Bat Wing", [], batWingTexture);
+        public static Material Crystal = new Material("Crystal", [], crystalTexture);
+        public static Material Eyeball = new Material("Eyeball", [], eyeballTexture);
+        public static Material Feather = new Material("Feather", [], featherTexture);
+        public static Material FrogLeg = new Material("Frog Leg", [], frogLegTexture);
+        public static Material Ice = new Material("Ice", [], iceTexture);
+        public static Material Ink = new Material("Ink", [], inkTexture);
+        public static Material Iron = new Material("Iron", [], ironTexture);
+        public static Material Moonlight = new Material("Moonlight", [], moonlightTexture);
+        public static Material Mushroom = new Material("Mushroom", [], mushroomTexture);
+        public static Material Paper = new Material("Paper", [], paperTexture);
+        public static Material Rock = new Material("Rock", [], rockTexture);
+        public static Material Salt = new Material("Salt", [], saltTexture);
+        public static Material Snow = new Material("Snow", [], snowTexture);
+        public static Material Sun = new Material("Sun", [], sunTexture);
+        public static Material Water = new Material("Water", [], waterTexture);
+        public static Material Wood = new Material("Wood", [], woodTexture);
+
 
         //Combined Materials
-        public static Material Mud = new Material("Mud", [Water, Earth]);
+        public static Material frogPotion = new Material("Frog Transmorphicator", [FrogLeg, Water]);
+        public static Material healingPotion = new Material("Health Bomb", [Sun, Water]);
+        public static Material spiritWorldPotion = new Material("Enter the Spirit World ahh potion", [Eyeball, Moonlight, Ink, Crystal]);
+        public static Material bloodMoonPotion = new Material("Blood Moon Appearus", [Sun, Water, Moonlight]);
+        public static Material invisPotion = new Material("Temp Invis in the Dark ahh potion", [BatWing, Water, Rock]);
+        public static Material floatyLightPotion = new Material("Floaty Lighty", [Sun, Fire, Paper]);
+        public static Material sightPotion = new Material("Potion of Seeing", [Eyeball, Moonlight, Water]);
+        public static Material barrierPotion = new Material("Magicus Barrius", [Iron, Salt, Rock]);
+        public static Material randomAnimalPotion = new Material("Turns into a Random Animall ahh potion", [Blood, Mushroom, Feather]);
+        public static Material pastPotion = new Material("User replay sounds or conversations from the past ahh potion", [Ink, Paper, Eyeball, Water]);
 
-        public static Material[] materials = { Junk, Fire, Water, Earth, Air, Mud };
 
-        public static Material Combine(Material[] inputMaterials)
+        public static Material[] materials = { Junk, Fire, Blood, BatWing, Crystal, Eyeball, Feather, FrogLeg, Ice, Ink, Iron, Moonlight, Mushroom, Paper, Rock, Salt, Snow, Sun, Water, Wood };
+        public static Material[] craftableMaterials = { frogPotion, healingPotion, spiritWorldPotion, bloodMoonPotion, invisPotion, floatyLightPotion, sightPotion, barrierPotion, randomAnimalPotion, pastPotion };
+
+        public static Material Combine(Material?[] inputMaterials)
         {
             // Combine the materials to create a new material
 
             // Sort the input materials alphabetically
-            Material[] orderedInputMaterials = inputMaterials.OrderBy(GetMaterialName).ToArray();
+            Material?[] orderedInputMaterials = inputMaterials.OrderBy(GetMaterialName).ToArray();
 
             foreach (Material material in materials)
             {
@@ -46,9 +101,12 @@ namespace Collaborative_2D_Game_Project
         }
 
         // Get the name of the material. This is used to sort the materials alphabetically
-        static string GetMaterialName(Material material)
+        static string GetMaterialName(Material? material)
         {
-            return material.name;
+            if (material is null)
+                return "";
+            else
+                return material.name;
         }
     }
 
